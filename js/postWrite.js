28f3662922fe17ed57906bff3f9828733d118f83
Gungod <gungod0601@gmail.com>
2022-03-18 16:58:39 +0900

    boardview work and project connection

--- js/postWrite.js
+++ js/postWrite.js
@@ -1,13 +1,5 @@
-  // aboutmeDelete 버튼 누를 시 동작
-  function deletePost(){
-    // var sel = document.getElementById("postId").value; // delete할 글의 id 가져옴
-    if(confirm('정말 삭제하시겠습니까?')){ // modal에서 OK 클릭시에 삭제 동작
-    //   document.getElementById("changeAboutmeForm").action = `/api/deleteAboutme/${sel}`; // delete 실행할 api 주소로 form action 주소 변경
-    //   document.getElementById("changeAboutmeForm").submit(); // delete 실행
-    }
-    // 취소시 do no-op
-  }
-  // aboutmeInsert 버튼 누를 시 동작
+
+// aboutmeInsert 버튼 누를 시 동작
   function submitPost(){
     document.getElementById("postProjectForm").action = `/api/insertPost/`;
     document.getElementById("postProjectForm").submit(); // postId option select시 지정된 form action api 주소로 submit 실행
